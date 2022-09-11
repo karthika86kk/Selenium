97ab1508917c24f7e161e3a2c4c2b498bec70d65
karthika86kk <110543107+karthika86kk@users.noreply.github.com>
2022-09-11 09:05:02 +0530

    Update DeleteLead.java

--- DeleteLead.java
+++ DeleteLead.java
@@ -9,7 +9,7 @@ public class DeleteLead extends ProjectSpecificMethods {
 	  @Test
 	public void runDeleteLead() throws InterruptedException{
 		
-		driver.get("http://leaftaps.com/opentaps/");
+		
 		
 		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
 		driver.findElement(By.id("password")).sendKeys("crmsfa");
